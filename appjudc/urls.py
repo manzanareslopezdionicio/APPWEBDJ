2190829e88dd36d894a43abfa767a2bff759bbd3
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-11-03 15:24:32 -0600

    login

--- appjudc/urls.py
+++ appjudc/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.login, name='login'),
+    path('registro/', views.registro, name='registro'),
 ]
--- appjudc/views.py
+++ appjudc/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
+
+def registro(request):
+    return render(request, 'registro.html')
